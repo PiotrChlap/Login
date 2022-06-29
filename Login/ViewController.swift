ebc7496ac77ca3f0f7a3368715cfc51a9a5ace10
Piotr Chłapiński <75984836+PiotrChlap@users.noreply.github.com>
2022-06-29 11:32:44 +0200

    Added changing of title

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var usernameField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.title = usernameField.text
+        
     }
 
 
